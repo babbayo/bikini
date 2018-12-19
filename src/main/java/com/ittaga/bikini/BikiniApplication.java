902a0ef71bcdae9351e7c6bc5b67e13b5ab1d91a
babbayo <na.babbayo@gmail.com>
2018-12-20 00:05:39 +0900

    init

---
+++ src/main/java/com/ittaga/bikini/BikiniApplication.java
@@ -0,0 +1,14 @@
+package com.ittaga.bikini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikiniApplication.class, args);
+	}
+
+}
+
